523ddb3fd3826525524723c62f4f6d85778a972c
tiolu22 <tolulopedeborah12@gmail.com>
2022-09-08 16:43:01 -0700

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /**
 * main - print the string in the put function
 *
@@ -10,5 +8,6 @@
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
