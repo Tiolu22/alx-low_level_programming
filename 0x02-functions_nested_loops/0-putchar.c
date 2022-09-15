be8d4dbe2afec049d5568892cb0f4113e12c7699
tiolu22 <tolulopedeborah12@gmail.com>
2022-09-14 19:05:39 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
 * main - print putchar
- * Return: always 0
+* Return: always 0
 */
 int main(void)
 {
@@ -14,6 +14,8 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
+_putchar('\n');
+
 _putchar('\n');
 return (0);
 }
