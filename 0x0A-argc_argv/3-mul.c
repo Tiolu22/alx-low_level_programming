6872feeaceb3fb6ee28d0c899ce536db38046c0f
tiolu22 <tolulopedeborah12@gmail.com>
2022-10-01 19:11:51 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
 * main - program that prints its name, followed by a new line
 * @argc: argument that counts argument input
@@ -9,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 int num_1, num_2, mul;
+
 if (argc != 3)
 printf("Error\n");
 else
